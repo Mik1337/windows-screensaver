5283ae4897fa0c409dcc279d2d1ed4b774f45170
mik <mik23@protonmail.ch>
2021-05-06 06:43:00 +0530

    Initial Commit

---
+++ windows screensaver/windows_screensaverView.h
@@ -0,0 +1,12 @@
+//
+//  windows_screensaverView.h
+//  windows screensaver
+//
+//  Created by mik on 06/05/21.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface windows_screensaverView : ScreenSaverView
+
+@end
---
+++ windows screensaver/windows_screensaverView.m
@@ -0,0 +1,51 @@
+//
+//  windows_screensaverView.m
+//  windows screensaver
+//
+//  Created by mik on 06/05/21.
+//
+
+#import "windows_screensaverView.h"
+
+@implementation windows_screensaverView
+
+- (instancetype)initWithFrame:(NSRect)frame isPreview:(BOOL)isPreview
+{
+    self = [super initWithFrame:frame isPreview:isPreview];
+    if (self) {
+        [self setAnimationTimeInterval:1/30.0];
+    }
+    return self;
+}
+
+- (void)startAnimation
+{
+    [super startAnimation];
+}
+
+- (void)stopAnimation
+{
+    [super stopAnimation];
+}
+
+- (void)drawRect:(NSRect)rect
+{
+    [super drawRect:rect];
+}
+
+- (void)animateOneFrame
+{
+    return;
+}
+
+- (BOOL)hasConfigureSheet
+{
+    return NO;
+}
+
+- (NSWindow*)configureSheet
+{
+    return nil;
+}
+
+@end
